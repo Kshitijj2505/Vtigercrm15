198e8a3ebf963697ec83d6b87563a4b930e71543
Kshitijj2505 <130834631+Kshitijj2505@users.noreply.github.com>
2023-05-23 15:30:00 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class compare
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter number");
+int a=sc.nextInt();
+System.out.println("enter number");
+int b=sc.nextInt();
+if(a>b)
+{
+System.out.println("largest number is="+a);
+return;
+}
+System.out.println("Largest number is="+0b);
+}
+}
